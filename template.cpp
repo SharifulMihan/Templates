c2d6ec4bb7db8495a6e4e073ad365ed56a2bf734
Shariful Mihan <mihanshariful@gmail.com>
2026-06-09 04:05:06 +0600

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define For(i,n) for(int i=0;i<n;i++)
+#define ll long long
+#define endl "\n"
+
+void solve() 
+{
+    
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}
