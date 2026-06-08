caa565b04313bdf4a4a0a3120b440291e7fa2fe6
Shariful Mihan <mihanshariful@gmail.com>
2026-06-09 03:43:23 +0600

    Add files via upload

---
+++ createPalindromeNum.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define endl "\n"
+
+ll createPalindromeNum(ll n,bool isOddLength){
+    ll palindrome = n;
+    ll origin;
+    if (isOddLength) origin = n/10;
+    else origin = n;
+
+    while(origin>0){
+        palindrome = palindrome*10 + origin%10;
+        origin /= 10;
+    }
+    return palindrome;
+}
+void printPalindromeAscendingOrder(ll n){
+    int range = (log10(n)+1)*2 - 1;
+
+    for (int i = 1; i <= range; i= 2*i + 1)
+    {
+        string binStr = bitset<8>(i).to_string();
+        int binInt = stoi(binStr);
+    
+        if(binInt > n) break;
+        for (int j = binInt-1; j <= binInt*9; ++j){
+            ll pal = createPalindromeNum(j,true);
+            if(pal > n) break;
+            cout<<pal<<endl;   
+        }
+        for (int j = binInt-1; j <= binInt*9; ++j){
+            ll pal = createPalindromeNum(j,false);
+            if(pal > n) break;
+            cout<<pal<<endl;
+        }
+    }
+}
+
+int main(){
+    printPalindromeAscendingOrder(300);
+}
