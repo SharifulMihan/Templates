e19f475bee169c87604c33bff7501cae8cc8bbc9
Shariful Mihan <mihanshariful@gmail.com>
2026-09-09 16:03:29 +0600

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define For(i,n) for(int i=0;i<n;i++)
 #define ll long long
 #define endl "\n"
+#define pb push_back
+#define yes "YES"
+#define no "NO"
+const int N = 2e6+5;
+const ll INF = 1e18;
+const int MOD = 1e9 + 7;
 
-void solve() 
+
+void solve()
 {
-    
+   
 }
 
-int32_t main() {
+int32_t main()
+{
     ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
     int t = 1;
     // cin >> t;
-    while (t--){
+    while (t--)
+    {
         solve();
     }
     return 0;
